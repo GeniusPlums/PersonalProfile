b7da94fe41a7f6bbbd2bd942e73d27c558fd7670
GeniusPlums <anish.adamane@gmail.com>
2024-05-24 19:13:22 +0530

    Initial commit

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "anish.adamane@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
