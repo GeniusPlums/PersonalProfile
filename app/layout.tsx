9fca4af68d1af46f096d831c8d03ca731b5ac63b
GeniusPlums <anish.adamane@gmail.com>
2024-06-17 21:17:17 +0530

    Updated logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Anish's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Anish Adamane",
+  description: "Anish's Portfolio",
 };
 
 export default function RootLayout({
